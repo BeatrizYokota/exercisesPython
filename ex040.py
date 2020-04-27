fb7b5ae8b860ef607cfe36bdb11b4bf40370e517
Beatriz Yokota <beatrizyokota97@gmail.com>
2020-04-27 15:54:13 -0300

    Testing a change

--- ex040.py
+++ ex040.py
@@ -1,6 +1,6 @@
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-media = (nota1 + nota2)/2
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+media = (n1 + n2)/2
 if media < 5:
     print("Reprovado!")
 elif media >= 5 and media < 7:
